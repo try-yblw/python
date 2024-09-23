c1a6fdede5baf0db67c0d89437c604c9db3c74a7
try-yblw <reraga27@gmail.com>
2024-09-23 16:46:46 +0300

    Create ex 1,2,3 in git.py

---
+++ ex 1,2,3 in git.py
@@ -0,0 +1,24 @@
+#Задание 1.
+integer_var = 7
+float_var = 7.7
+string_var = "SEMb"
+list_var = [7,7.7,77,777]
+tuple_var = (7,"SEMb" )
+dict_var = {"словарь с ключами и значениями"}
+set_var = (1,3,5,4,2)
+bool_var = 3>0
+print(integer_var,float_var,string_var,list_var,tuple_var,dict_var,set_var,bool_var,sep='; ' )
+#Задание 2.
+name = "Yura"
+age = 17
+Average_grade = 4.1
+Favorite_movies = ["zeloniy slonik", "zelonaya knigga", "arhiv"]
+personal_info = ("Olar",1362,4.1)
+skills = {"ymniy" : "dobriy"}
+visited_countries = {"Türkiye","Egypt","Peter"}
+has_driver_license = 0>1
+#Задание 3.
+ko1 = "intel"
+ko2 = 16
+ko3 = 74.79
+print(f'Название процессора: {ko1} \nОбъём оперативной памяти: {ko2} Гб \nСкорость интернета: {ko3} Мбит/с')
